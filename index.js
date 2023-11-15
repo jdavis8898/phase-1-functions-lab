c9e40d00f57fbbccca6c92af3eb75e7141622ec9
Jeffrey Davis <j.davis081248@gmail.com>
2023-11-15 01:07:56 -0600

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street)
+{
+    let disBlocks = Math.abs(42 - street)
+
+    return disBlocks
+}
+
+function distanceFromHqInFeet(street)
+{
+    let disFeet =  distanceFromHqInBlocks(street) * 264
+    
+    return disFeet
+}
+
+function distanceTravelledInFeet(start, destination)
+{
+    let disTrav = Math.abs(distanceFromHqInFeet(destination) - distanceFromHqInFeet(start))
+
+    return disTrav
+}
+
+function calculatesFarePrice(start, destination)
+{
+    let disTravel = distanceTravelledInFeet(start, destination)
+    let fare
+
+    if (disTravel < 400)
+    {
+        fare = 0
+
+        return fare
+    }
+
+    else if (disTravel >= 400 && disTravel <= 2000)
+    {
+        fare = (disTravel - 400) * .02
+
+        return fare
+    }
+
+    else if (disTravel > 2000 && disTravel < 2500)
+    {
+        fare = 25
+
+        return fare
+    }
+
+    else
+    {
+        fare = 'cannot travel that far'
+
+        return fare
+    }
+}
